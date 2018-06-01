f1d17fc6a8a6c7a1708bbc42a162afb8cd8166d6
wlor0623 <34371116+wlor0623@users.noreply.github.com>
2018-06-02 00:13:02 +0800

    初始化

--- bilibili/bilibili.js
+++ bilibili/bilibili.js
@@ -1,10 +1,9 @@
-const version = 1.2;
+// https://github.com/wlor0623/jsbox/edit/master/bilibili/bilibili.js
+const version = 1.0;
 //检测扩展更新
 scriptVersionUpdate();
-
-const vedioListUrl = "https://app.bilibili.com/x/feed/index?build=6190&network=wifi&platform=ios"//列表
-
-
+const vedioListUrl =
+  "https://app.bilibili.com/x/feed/index?build=6190&network=wifi&platform=ios"; //列表
 //https://api.bilibili.com/x/v2/reply?oid=6666666&plat=3&pn=3&ps=20&sort=0&type=1 评论
 //https://api.bilibili.com/x/article/categories     //分类?
 // https://bangumi.bilibili.com/appindex/cinema/fall?platform=ios&region=index  //落 首页 电影
@@ -15,19 +14,17 @@ const vedioListUrl = "https://app.bilibili.com/x/feed/index?build=6190&network=w
 // https://app.bilibili.com/x/v2/view/page?access_key=611aaad5fb61b6494ce39b6d6ff9d4f3&actionKey=appkey&aid=23977784&appkey=27eb53fc9058f8c3&build=6190&device=phone&mobi_app=iphone&platform=ios&sign=f1de6b939e8c99cca1085f354436690f&ts=1527855897
 let listData = [];
 let firstLoad = true;
-
+let aid=0;
 getlistData();
-s1.hdslb.com
 function getlistData(sender) {
   $ui.loading("加载中");
   $http.get({
     url: vedioListUrl,
-    handler: function (resp) {
+    handler: function(resp) {
       var data = resp.data;
       if (data.code == 0) {
         var data = data.data;
         console.log(data);
-
         // var bannerData = data[0].banner_item; //轮播数据
         // let banner_img = [];
         // for (let i = 0; i < bannerData.length; i++) {
@@ -48,11 +45,14 @@ function getlistData(sender) {
                 text: data[i].title //标题
               },
               cover_duration_play: {
-                text:data[i].play > 10000?Math.ceil(data[i].play / 1000) / 10 + "万":" " + data[i].play + "次" //播放量
+                text:
+                  data[i].play > 10000
+                    ? Math.ceil(data[i].play / 1000) / 10 + "万"
+                    : " " + data[i].play + "次" //播放量
               },
               cover_duration_duration: {
-                text: s_to_m_s(data[i].duration)//时长
-              }, 
+                text: s_to_m_s(data[i].duration) //时长
+              },
               cover_item_image: {
                 src: data[i].cover //封面
               },
@@ -71,8 +71,7 @@ function getlistData(sender) {
               cover_item_danmu: {
                 text: data[i].danmaku //弹幕数量
               }
-             
-            }
+            };
             listData.push(obj);
           }
         }
@@ -87,15 +86,15 @@ function getlistData(sender) {
         }
       }
     }
-  })
+  });
 }
 
-//轮播图      
+//渲染
 function render(listData) {
   $ui.render({
     type: "view",
     props: {
-      bgcolor: $color("#f3f4f4"),
+      bgcolor: $color("#f3f4f4")
     },
     views: [
       //   {
@@ -114,7 +113,7 @@ function render(listData) {
 
       //     },
       //   }
-      // }, 
+      // },
       {
         type: "matrix", //九宫格
         props: {
@@ -127,200 +126,315 @@ function render(listData) {
           square: false,
           radius: 10, //圆角,
           template: {
-            views: [{
-              type: "view", //单个盒子
-              props: {
-                bgcolor: $color("#ffffff"),
-                textColor: $color("#555555"),
-                radius: 10,
-                borderWidth: 1,
-                borderColor: $color("#e1e1e1"),
-              },
-              views: [{
-                type: "image",
+            views: [
+              {
+                type: "view", //单个盒子
                 props: {
-                  id: 'cover_item_image',
+                  bgcolor: $color("#ffffff"),
+                  textColor: $color("#555555"),
+                  radius: 10,
+                  borderWidth: 1,
+                  borderColor: $color("#e1e1e1")
                 },
-                layout: function (make, view) {
-                  make.top.left.right.equalTo(0);
-                  make.height.equalTo(100);
-                },
-                events: {
-                  tapped(sender) {
-                    console.log(sender)
-                  },
-                }
-              }],
-              layout: function (make, view) {
-                make.top.bottom.left.right.equalTo(0);
-                make.height.equalTo(view.super);
-                make.width.equalTo(view.super);
-              }
-            }, {
-              type: "view",
-              props: {
-                font: $font(10),
-              },
-              views: [{
-                type: "label", //半透明注释内容 (视频时间)
-                props: {
-                  font: $font(12),
-                  lines: 0,
-                  textColor: $color('#f5f5f5'),
-                },
-                views: [{
-                  type: "label", // 视频时长
-                  props: {
-                    id: "cover_duration_duration",
-                    font: $font(12),
-                    radius: 5,
-                    bgcolor: $rgba(0, 0, 0, 0.5),
-                    textColor: $color('#f5f5f5'),
-                    align: $align.center,
-                  },
-                  layout: function (make, view) {
-                    make.top.equalTo(0);
-                    make.right.equalTo(0);
-                    make.height.equalTo(20);
-                    make.width.equalTo(40)
+                views: [
+                  {
+                    type: "image",
+                    props: {
+                      id: "cover_item_image"
+                    },
+                    layout: function(make, view) {
+                      make.top.left.right.equalTo(0);
+                      make.height.equalTo(100);
+                    }
                   }
-                }],
-                layout: function (make, view) {
-                  make.top.equalTo(-20);
-                  make.height.equalTo(20);
-                  make.width.equalTo(view.super)
+                ],
+                layout: function(make, view) {
+                  make.top.bottom.left.right.equalTo(0);
+                  make.height.equalTo(view.super);
+                  make.width.equalTo(view.super);
                 }
-              }, {
-                type: "label", //视频标题
+              },
+              {
+                type: "view",
                 props: {
-                  id: 'cover_duration_title',
-                  font: $font(12),
-                  lines: 0,
-                  align: $align.center
+                  font: $font(10)
                 },
-                layout: function (make, view) {
-                  make.left.right.inset(5);
-                  make.top.equalTo(10);
-                  make.height.equalTo(30);
+                views: [
+                  {
+                    type: "label", //半透明注释内容 (视频时间)
+                    props: {
+                      font: $font(12),
+                      lines: 0,
+                      textColor: $color("#f5f5f5")
+                    },
+                    views: [
+                      {
+                        type: "label", // 视频时长
+                        props: {
+                          id: "cover_duration_duration",
+                          font: $font(12),
+                          radius: 5,
+                          bgcolor: $rgba(0, 0, 0, 0.5),
+                          textColor: $color("#f5f5f5"),
+                          align: $align.center
+                        },
+                        layout: function(make, view) {
+                          make.top.equalTo(0);
+                          make.right.equalTo(0);
+                          make.height.equalTo(20);
+                          make.width.equalTo(40);
+                        }
+                      }
+                    ],
+                    layout: function(make, view) {
+                      make.top.equalTo(-20);
+                      make.height.equalTo(20);
+                      make.width.equalTo(view.super);
+                    }
+                  },
+                  {
+                    type: "label", //视频标题
+                    props: {
+                      id: "cover_duration_title",
+                      font: $font(12),
+                      lines: 0,
+                      align: $align.center
+                    },
+                    layout: function(make, view) {
+                      make.left.right.inset(5);
+                      make.top.equalTo(10);
+                      make.height.equalTo(30);
+                    }
+                  },
+                  {
+                    type: "label", //视频up主
+                    props: {
+                      id: "cover_item_name",
+                      font: $font(12),
+                      align: $align.left
+                    },
+                    layout: function(make, view) {
+                      make.left.right.inset(5);
+                      make.top.equalTo(40);
+                      make.height.equalTo(30);
+                    }
+                  },
+                  {
+                    type: "label", //视频发布时间
+                    props: {
+                      id: "cover_item_ctime",
+                      font: $font(12),
+                      align: $align.right
+                    },
+                    layout: function(make, view) {
+                      make.left.right.inset(5);
+                      make.top.equalTo(40);
+                      make.height.equalTo(30);
+                    }
+                  },
+                  {
+                    type: "label", //视频标签
+                    props: {
+                      id: "cover_item_tag_name",
+                      font: $font(12),
+                      bgcolor: $color("#eee"),
+                      align: $align.center,
+                      radius: 10
+                    },
+                    layout: function(make, view) {
+                      make.top.equalTo(70);
+                      make.left.inset(5);
+                      make.height.equalTo(20);
+                      make.width.greaterThanOrEqualTo(40);
+                    }
+                  },
+                  {
+                    type: "label", // 播放量
+                    props: {
+                      id: "cover_duration_play",
+                      font: $font(12),
+                      bgcolor: $color("#eee"),
+                      align: $align.center,
+                      radius: 10
+                    },
+                    layout: function(make, view) {
+                      make.top.equalTo(70);
+                      make.right.inset(5);
+                      make.height.equalTo(20);
+                      make.width.greaterThanOrEqualTo(40);
+                    }
+                  }
+                ],
+                layout: function(make, view) {
+                  make.top.equalTo(100);
+                  make.left.right.bottom.equalTo(0);
                 }
-              }, {
-                type: "label", //视频up主
-                props: {
-                  id: 'cover_item_name',
-                  font: $font(12),
-                  align: $align.left
-                },
-                layout: function (make, view) {
-                  make.left.right.inset(5);
-                  make.top.equalTo(40);
-                  make.height.equalTo(30);
-                }
-              }, {
-                type: "label", //视频发布时间
-                props: {
-                  id: 'cover_item_ctime',
-                  font: $font(12),
-                  align: $align.right
-                },
-                layout: function (make, view) {
-                  make.left.right.inset(5);
-                  make.top.equalTo(40);
-                  make.height.equalTo(30);
-                }
-              }, {
-                type: "label", //视频标签
-                props: {
-                  id: 'cover_item_tag_name',
-                  font: $font(12),
-                  bgcolor: $color('#eee'),
-                  align: $align.center,
-                  radius: 10,
-                },
-                layout: function (make, view) {
-                  make.top.equalTo(70);
-                  make.left.inset(5);
-                  make.height.equalTo(20);
-                  make.width.greaterThanOrEqualTo(40);
-                }
-              }, {
-                type: "label", // 播放量
-                props: {
-                  id: "cover_duration_play",
-                  font: $font(12),
-                  bgcolor: $color('#eee'),
-                  align: $align.center,
-                  radius: 10,
-                },
-                layout: function (make, view) {
-                  make.top.equalTo(70);
-                  make.right.inset(5);
-                  make.height.equalTo(20);
-                  make.width.greaterThanOrEqualTo(40);
-                }
-              }],
-              layout: function (make, view) {
-                make.top.equalTo(100);
-                make.left.right.bottom.equalTo(0);
               }
-            }]
+            ]
           }
         },
-        layout: function (make, view) {
+        layout: function(make, view) {
           make.top.left.bottom.right.equalTo(0);
         },
         events: {
-          didReachBottom: function (sender) {
+          didReachBottom: function(sender) {
             getlistData(sender);
           },
-          didSelect: function (sender, indexPath, data) {
-
+          didSelect: function(sender, indexPath, data) {
+            //打开网页
+            aid = data.cover_item_param.text;
+            $ui.push({
+              views: [
+                {
+                  type: "web",
+                  props: {
+                    showsProgress: true,
+                    inlineMedia: true,
+                    scrollEnabled: false,
+                    url: `https://m.bilibili.com/video/av${aid}.html`,
+                    style: ".index__videoPage__src-videoPage-{padding-top:0px;}"//去除头部app推广
+                  },
+                  layout: function(make, view) {
+                    make.height.equalTo(260);
+                    make.width.equalTo(view.super);
+                  }
+                },
+                {
+                  type: "button",
+                  props: {
+                    title: "保存封面"
+                  },
+                  layout: function(make, view) {
+                    make.top.equalTo(280);
+                    make.width.equalTo(160);
+                    make.height.equalTo(40);
+                    make.left.equalTo(20);
+                  },
+                  events: {
+                    tapped: function(sender) {
+                      $ui.toast("正在获取..");
+                      $ui.loading(true);
+                      $http.get({
+                        url: "http://www.galmoe.com/t.php?aid=" + aid,
+                        handler: function(resp) {
+                          var data = resp.data;
+                          $ui.loading(false);
+                          console.log(data);
+                          if (data.result == 1) {
+                            let url = data.url;
+                            saveImage(url);
+                          } else {
+                            $ui.alert("封面获取失败！:(");
+                          }
+                        }
+                      });
+                    }
+                  }
+                },
+                {
+                  type: "button",
+                  props: {
+                    title: "跳转客户端",
+                    bgcolor: $color("#da5380")
+                  },
+                  layout: function(make, view) {
+                    make.top.equalTo(280);
+                    make.width.equalTo(160);
+                    make.height.equalTo(40);
+                    make.right.equalTo(-20);
+                  },
+                  events: {
+                    tapped: function(sender) {
+                      let urlScheme="bilibili://?av="+aid;
+                      console.log(urlScheme);
+                      $app.openURL(urlScheme);
+                    }
+                  }
+                }
+              ],
+              layout: function(make, view) {
+                make.top.left.bottom.right.equalTo(0);
+              }
+            });
           }
         }
       }
     ]
-  })
+  });
 }
 
 //秒数转为 00:00
 function s_to_m_s(s) {
   let min = Math.floor(s / 60);
-  min = min < 10 ? '0' + min : min;
-  let sec = (s % 60);
-  sec = sec < 10 ? '0' + sec : sec;
-  return min + ":" + sec
+  min = min < 10 ? "0" + min : min;
+  let sec = s % 60;
+  sec = sec < 10 ? "0" + sec : sec;
+  return min + ":" + sec;
 }
 //时间戳转换
 function ms_To_ymd(timestamp) {
   let date = new Date(timestamp * 1000); //时间戳为10位需*1000，时间戳为13位的话不需乘1000
-  M = (date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1) + '-';
-  D = date.getDate()<10?"0"+date.getDate():date.getDate() + ' '
+  M =
+    (date.getMonth() + 1 < 10
+      ? "0" + (date.getMonth() + 1)
+      : date.getMonth() + 1) + "-";
+  D = date.getDate() < 10 ? "0" + date.getDate() : date.getDate() + " ";
 
-  return M + D
+  return M + D;
 }
 
 //检测扩展更新
 function scriptVersionUpdate() {
-    $http.get({
-      url: "https://raw.githubusercontent.com/wlor0623/jsbox/master/bilibili/updateInfo.js",
-      handler: function (resp) {
-        let afterVersion = resp.data.version;
-        let msg = resp.data.msg;
-        if (afterVersion > version) {
-          $ui.alert({
-            title: "检测到新的版本！V" + afterVersion,
-            message: "是否更新?\n更新完成后请退出至扩展列表重新启动新版本。\n" + msg,
-            actions: [{
+  $http.get({
+    url:"https://raw.githubusercontent.com/wlor0623/jsbox/master/bilibili/updateInfo.js",
+    handler: function(resp) {
+      let afterVersion = resp.data.version;
+      let msg = resp.data.msg;
+      if (afterVersion > version) {
+        $ui.alert({
+          title: "检测到新的版本！V" + afterVersion,
+          message:
+            "是否更新?\n更新完成后请退出至扩展列表重新启动新版本。\n" + msg,
+          actions: [
+            {
               title: "更新",
-              handler: function () {
-                let url = "jsbox://install?url=https://raw.githubusercontent.com/wlor0623/jsbox/master/bilibili/bilibili.js&name=哔哩哔哩封面提取v" + afterVersion + "&icon=icon_014.png";
+              handler: function() {
+                let url =
+                  "jsbox://install?url=https://raw.githubusercontent.com/wlor0623/jsbox/master/bilibili/bilibili.js&name=bilibili" +
+                  afterVersion +
+                  "&icon=icon_014.png";
                 $app.openURL(encodeURI(url));
-                $app.close()
+                $app.close();
               }
-            }, {
+            },
+            {
               title: "取消"
-            }]
-          })
-        }
+            }
+          ]
+        });
       }
-    })
-  }
+    }
+  });
+}
+// 保存图片
+function saveImage(imgUrl) {
+  $http.download({
+    url: imgUrl,
+    progress: function (bytesWritten, totalBytes) {
+      var percentage = bytesWritten * 1.0 / totalBytes
+    },
+    handler: function (resp) {
+      $photo.save({
+        data: resp.data,
+        handler: function (success) {
+          $ui.loading(false);
+          if (success == 1) {
+            $ui.alert("封面保存成功");
+          }else{
+            $ui.alert("保存失败!");
+          }
+        }
+      })
+    }
+  })
+}

231d8a36c8248c43ce23be3c8bb868b6e2a78ac3
wlor0623 <34371116+wlor0623@users.noreply.github.com>
2018-07-21 17:20:22 +0800

    Update chelaile.js

--- chelaile/chelaile.js
+++ chelaile/chelaile.js
@@ -1,17 +1,17 @@
-const version = 1.1; //版本号
+const version = 1.2; //版本号
 //检测扩展更新
 scriptVersionUpdate();
-$app.validEnv = $env.app
+$app.validEnv = $env.app;
 var tip = "";
 var weather = "";
 var pageTitle = "";
 var mainColor = "#508aeb";
-var screenHeight = $device.info.screen.height - 40;
+var screenHeight = $device.info.screen.height -40;
 getLocation();
 
 function getLocation() {
   $location.fetch({
-    handler: function (resp) {
+    handler: function(resp) {
       let lat = resp.lat;
       let lng = resp.lng;
       $location.stopUpdates();
@@ -35,7 +35,7 @@ async function getCity(lat, lng) {
 function getWeather(cityName) {
   $http.get({
     url: `https://www.sojson.com/open/api/weather/json.shtml?city=${cityName}`,
-    handler: function (resp) {
+    handler: function(resp) {
       let data = resp.data;
       tip = `${data.data.forecast[0].notice}`;
       weather = `${data.data.forecast[0].type} : ${data.data.wendu}℃`;
@@ -46,27 +46,28 @@ function getWeather(cityName) {
 }
 
 function renderMap(lat, lng, cityId, cityName) {
-  const url = `http://web.chelaile.net.cn/ch5/index.html?utm_source=webapp_meizu_map&gpstype=wgs&src=webapp_meizu_map&utm_medium=menu&hideFooter=1&cityName=${cityName}&cityId=${cityId}&supportSubway=1&cityVersion=0&lat=${lat}&lng=${lng}#!/linearound`;
+  const url = `http://web.chelaile.net.cn/ch5/index.html?showFav=1&switchCity=0&utm_source=webapp_meizu_map&showTopLogo=0&gpstype=wgs&src=webapp_meizu_map&utm_medium=menu&showHeader=1&hideFooter=1&cityName=${cityName}&cityId=${cityId}&supportSubway=1&cityVersion=0&lat=${lat}&lng=${lng}#!/linearound`;
   $ui.render({
     props: {
       type: "view",
       navBarHidden: true,
       bgcolor: $color(mainColor)
     },
-    views: [{
+    views: [
+      {
         type: "label",
         props: {
           text: "",
-          id: "close",
+          id: "close"
         },
-        layout: function (make) {
+        layout: function(make) {
           make.top.inset(30);
           make.right.inset(0);
           make.height.equalTo(30);
           make.width.equalTo(80);
         },
         events: {
-          tapped: function () {
+          tapped: function() {
             $app.close(0);
           }
         }
@@ -81,17 +82,18 @@ function renderMap(lat, lng, cityId, cityName) {
           align: $align.center,
           font: $font(20)
         },
-        layout: function (make) {
+        layout: function(make) {
           make.top.inset(30);
-          make.left.inset(15);
+          make.centerX.equalTo(0)
           make.height.equalTo(30);
         },
         events: {
-          tapped: function () {
+          tapped: function() {
             getLocation();
           }
         }
       },
+      
       {
         type: "label",
         props: {
@@ -102,7 +104,7 @@ function renderMap(lat, lng, cityId, cityName) {
           textColor: $color("#fff"),
           align: $align.center
         },
-        layout: function (make, view) {
+        layout: function(make, view) {
           make.width.equalTo(view.super);
           make.height.equalTo(30);
           make.top.equalTo(60);
@@ -118,26 +120,10 @@ function renderMap(lat, lng, cityId, cityName) {
           textColor: $color("#fff"),
           align: $align.center
         },
-        layout: function (make, view) {
+        layout: function(make, view) {
           make.width.equalTo(view.super);
           make.height.equalTo(30);
           make.top.equalTo(90);
-        }
-      },
-      {
-        type: "label",
-        props: {
-          id: "Smile",
-          hidden: true,
-          text: "(σ・ω・)σ(",
-          lines: 0,
-          textColor: $color("#fff"),
-          align: $align.center
-        },
-        layout: function (make, view) {
-          make.width.equalTo(view.super);
-          make.height.equalTo(30);
-          make.top.equalTo(120);
         }
       },
       {
@@ -145,24 +131,23 @@ function renderMap(lat, lng, cityId, cityName) {
         props: {
           url: url,
           id: "webView",
-          style: `.f-11,.f-12,.f-13{font-size:14px !important;}.container{max-width:none}.page-list .ico-chelaile-container{display:none;}.page-list .switch-city{display:none;}.page-list .div-imitate-search-ui{padding:9px;}.around-refresh{background-color: ${mainColor}}.page-list .div-imitate-input{text-align: center;}`
+          style: `.container{max-width:none}.page-list .switch-city{display:none;}.page-list .div-imitate-search-ui{padding:9px;}.around-refresh{background-color: ${mainColor}}.page-list .div-imitate-input{text-align: center;}`
         },
-        layout: function (make, view) {
-          make.top.equalTo($('cityName').bottom);
+        layout: function(make, view) {
+          make.top.equalTo($("cityName").bottom);
           make.left.right.equalTo(0);
-          make.height.equalTo(screenHeight)
+          make.height.equalTo(screenHeight);
         },
         events: {
-          didFinish: function (sender) {
-            $("Smile").hidden = false;
+          didFinish: function(sender) {
             $("tips").hidden = false;
             $("weather").hidden = false;
             $delay(1, function() {
               sender.transparent = true;
-            })
+            });
           }
         }
-      },
+      }
     ]
   });
 }
@@ -170,17 +155,19 @@ function renderMap(lat, lng, cityId, cityName) {
 //检测扩展更新
 function scriptVersionUpdate() {
   $http.get({
-    url: "https://raw.githubusercontent.com/wlor0623/jsbox/master/chelaile/updateInfo.js",
-    handler: function (resp) {
+    url:
+      "https://raw.githubusercontent.com/wlor0623/jsbox/master/chelaile/updateInfo.js",
+    handler: function(resp) {
       let afterVersion = resp.data.version;
       let msg = resp.data.msg;
       if (afterVersion > version) {
         $ui.alert({
           title: `检测到新的版本！V ${afterVersion}`,
           message: `是否更新?\n更新完成后请退出至扩展列表重新启动新版本。\n  ${msg}`,
-          actions: [{
+          actions: [
+            {
               title: "更新",
-              handler: function () {
+              handler: function() {
                 let url = `jsbox://install?url=https://raw.githubusercontent.com/wlor0623/jsbox/master/chelaile/chelaile.js&name=车来了网页版&icon=icon_087.png`;
                 $app.openURL(encodeURI(url));
                 $app.close();
